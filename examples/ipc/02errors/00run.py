b04ad92c1e51682441296694069d8d71bcfa0656
podhmo <ababjam61+github@gmail.com>
2020-01-26 19:07:53 +0900

    chore: add examples

---
+++ examples/ipc/02errors/00run.py
@@ -0,0 +1,19 @@
+from handofcats import as_subcommand
+from commands import executor
+from commands import producer, consumer
+
+
+@as_subcommand
+def run():
+    with executor:
+        n = 2
+
+        for uid in range(n):
+            endpoint = executor.create_endpoint(uid=uid)
+            executor.spawn(producer, endpoint=endpoint)
+            executor.spawn(consumer, endpoint=endpoint)
+
+        executor.wait()
+
+
+as_subcommand.run()
--- examples/ipc/02errors/commands.py
+++ examples/ipc/02errors/commands.py
@@ -1,5 +1,4 @@
 import time
-from handofcats import as_subcommand
 import minitask
 from minitask.port import fake as port
 from minitask.executor.threaded import Executor
@@ -7,36 +6,23 @@ from minitask.executor.threaded import Executor
 executor = Executor()
 
 
-@as_subcommand
-def run():
-    with executor:
-        n = 2
-
-        for uid in range(n):
-            endpoint = executor.create_endpoint(uid=uid)
-            executor.spawn(producer, endpoint=endpoint)
-            executor.spawn(consumer, endpoint=endpoint)
-
-        executor.wait()
-
-
 @executor.register
-def producer(*, endpoint: str):
+def producer(*, endpoint: str, ng: bool = False):
     import os
 
     ipc = minitask.IPC(port=port)
     pid = os.getpid()
     with ipc.serve(endpoint) as x:
         for i in range(5):
-            if i == 2:
-                1 / 0
             # msgpackrpc not support kwargs
             x.write("say", args=["hello", [pid]])
             time.sleep(0.1)
+            if ng:
+                1 / 0
 
 
 @executor.register
-def consumer(*, endpoint: str):
+def consumer(*, endpoint: str, ng: bool = False):
     import os
 
     ipc = minitask.IPC(port=port)
@@ -44,6 +30,5 @@ def consumer(*, endpoint: str):
     with ipc.connect(endpoint) as x:
         for msg in x:
             print("got", msg.unique_id, msg.method, msg.args, "with", pid)
-
-
-as_subcommand.run()
+            if ng:
+                1 / 0
